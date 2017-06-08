c30f8007187f8b2a54a36fb145b8bd5c4deb47f0
Ricardo Krause <krr32300@othr.onmicrosoft.com>
2017-06-08 13:58:41 +0200

    add a boolean to style converter

--- Integreat/Integreat.Shared/Converters/HtmlSourceConverter.cs
+++ Integreat/Integreat.Shared/Converters/HtmlSourceConverter.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Security;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 
 namespace Integreat.Shared.Converters
